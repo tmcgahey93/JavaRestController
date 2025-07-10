d456d956236af364809639da0b6e524b9747d730
tmcgahey93 <tmcgahey93@gmail.com>
2025-07-09 21:43:42 -0400

    Add Actuator

--- src/main/java/com/javarestcontroller/JavaRestController.java
+++ src/main/java/com/javarestcontroller/JavaRestController.java
@@ -1,6 +1,6 @@
 package com.javarestcontroller;
-
-
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +10,12 @@ public class JavaRestController {
     @RequestMapping
     public String javaRestController() {
         return "Troys JavaRestController Running";
+    }
+
+    @GetMapping("/expensive")
+    @Timed(value = "expensive.operation", description = "Time spent on expensive operation")
+    public String expensiveCall() throws InterruptedException {
+        Thread.sleep(500);  // simulate delay
+        return "Done";
     }
 }
