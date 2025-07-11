67eca0914edef344851aa1b12aa0544b78cf43ca
tmcgahey93 <tmcgahey93@gmail.com>
2025-07-10 20:57:33 -0400

    Testing Metrics

--- src/main/java/com/javarestcontroller/JavaRestController.java
+++ src/main/java/com/javarestcontroller/JavaRestController.java
@@ -1,15 +1,13 @@
 package com.javarestcontroller;
-import io.micrometer.core.annotation.Timed;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JavaRestController {
 
-    @RequestMapping("/")
-    @Timed(value = "RESTCALL", description = "RESTCALL")
-    public String javaRestController() {
-        ExpensiveClass.doWork();
-        return "Troys JavaRestController Running";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestController.class, args);
+	}
+
 }
---
+++ src/main/java/com/javarestcontroller/JavaRestControllerHandler.java
@@ -0,0 +1,15 @@
+package com.javarestcontroller;
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JavaRestControllerHandler {
+
+    @RequestMapping("/")
+    @Timed(value = "RESTCALL", description = "RESTCALL")
+    public String javaRestController() {
+        ExpensiveClass.doWork();
+        return "Troys JavaRestController Running";
+    }
+}
